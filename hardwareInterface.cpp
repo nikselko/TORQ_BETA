f06cd827a2dccdfb769e97495da6caff98e5793d
Nikita S <46798613+nikselko@users.noreply.github.com>
2021-05-28 22:44:33 +0100

    Update hardwareInterface.cpp

--- hardwareInterface.cpp
+++ hardwareInterface.cpp
@@ -1,6 +1,6 @@
 /******************************************************************************
 *                                                                             *
-*   Program : FileLogger                                                      *
+*   Program : TORQ beta                                                       *
 *                                                                             *
 *   FILE : hardwareInterface.h                                                *
 *                                                                             *
